cf944bdd01ec7883edcf6ce5f19b7c97df82c134
Libertas007 <1adamsvoboda@gmail.com>
2025-09-24 09:23:05 +0200

    added a calculator

--- Lecture3/Lecture3/Program.cs
+++ Lecture3/Lecture3/Program.cs
@@ -1,3 +1,97 @@
-﻿Console.WriteLine("== Kalkulačka ==");
+﻿Console.WriteLine("== Kalkulačka ==\n");
+
+bool exit = false;
+
+while (!exit) 
+{
+    Console.WriteLine("| Podporované operace: +, -, *, /, exit");
+
+    Console.Write("> Zadejte operaci: ");
+
+    string operation = (Console.ReadLine() ?? string.Empty).Trim();
+
+    if (operation.ToLower() == "exit")
+    {
+        exit = true;
+        continue;
+    }
+    
+    if (operation == string.Empty || !(new[] {"+", "-", "*", "/"}).Contains(operation))
+    {
+        ErrorWriteLine("! Neplatná operace, zkuste to znovu.");
+        Console.WriteLine("|");
+        continue;
+    }
 
 
+    Console.WriteLine("|");
+
+    double a, b;
+
+    while (true)
+    {
+        Console.Write("> Zadejte první číslo: ");
+        string inputA = Console.ReadLine() ?? string.Empty;
+
+        if (!double.TryParse(inputA, out a))
+        {
+            ErrorWriteLine("! Neplatný vstup, zadejte číslo znovu: ");
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    while (true)
+    {
+        Console.Write("> Zadejte druhé číslo: ");
+        string inputB = Console.ReadLine() ?? string.Empty;
+
+        if (!double.TryParse(inputB, out b) || (operation == "/" && b == 0))
+        {
+            ErrorWriteLine("! Neplatný vstup, zadejte číslo znovu: ");
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    double c = 0;
+
+    switch (operation)
+    {
+        case "+":
+            c = a + b;
+            break;
+        case "-":
+            c = a - b;
+            break;
+        case "*":
+            c = a * b;
+            break;
+        case "/":
+            c = a / b;
+            break;
+    }
+
+    Console.WriteLine("|");
+
+    Console.Write(
+        $"| a {operation} b = {a} {operation} {b} = "
+    );
+    Console.BackgroundColor = ConsoleColor.DarkGray;
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine($" {c} ");
+    Console.ResetColor();
+    Console.WriteLine("|");
+}
+
+void ErrorWriteLine(string message)
+{
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine(message);
+    Console.ResetColor();
+}
