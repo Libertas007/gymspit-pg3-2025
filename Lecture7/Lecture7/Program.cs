3d663b9cbf59d5f6ce43c1efc215e8ccd195b5f9
Libertas007 <1adamsvoboda@gmail.com>
2025-11-05 09:26:34 +0100

    voluntary assignment completed

--- Lecture7/Lecture7/Program.cs
+++ Lecture7/Lecture7/Program.cs
@@ -38,7 +38,34 @@
                 case "lookup":
                     LookupStudent();
                     break;
+                case "count":
+                    CountStudents();
+                    break;
+                case "delete":
+                    DeleteStudent();
+                    break;
+                case "help":
+                    Help();
+                    break;
+                case "size":
+                    Console.WriteLine("< 128");
+                    break;
+                case "exit":
+                    Environment.Exit(0);
+                    break;
             }
+        }
+
+        public static void Help()
+        {
+            Console.WriteLine("| add - add a student");
+            Console.WriteLine("| count - get the number of students");
+            Console.WriteLine("| delete - delete a student");
+            Console.WriteLine("| exit - quit the program");
+            Console.WriteLine("| help - print this help");
+            Console.WriteLine("| list - get the full list of students");
+            Console.WriteLine("| lookup - find a student by name");
+            Console.WriteLine("| size - get the max size of the list (aka n - 1 is the last available index)");
         }
 
         public static void AddStudent()
@@ -49,14 +76,21 @@
             Console.Write("| Enter student id: ");
             string idS = Console.ReadLine() ?? "";
 
+
             if (!int.TryParse(idS, out var id)) {
-                Console.WriteLine("< Invalid number");
-                return;
+                if (idS.Trim().Length == 0) {
+                    id = Array.FindIndex(students, string.IsNullOrEmpty);
+                }
+                else
+                {
+                    Console.WriteLine("! Invalid number");
+                    return;
+                }
             }
 
             if (!string.IsNullOrEmpty(students[id]))
             {
-                Console.WriteLine("< ID occupied");
+                Console.WriteLine("! ID occupied");
                 return;
             }
         
@@ -81,7 +115,43 @@
             Console.Write("| Enter student name: ");
             string name = Console.ReadLine() ?? "";
 
-            Console.WriteLine($"< {students.Contains(name)}");
+            Console.WriteLine($"< {Array.IndexOf(students, name)}");
+        }
+
+        public static void DeleteStudent()
+        {
+            Console.Write("| Enter student id: ");
+            string idS = Console.ReadLine() ?? "";
+
+            if (!int.TryParse(idS, out var id))
+            {
+                Console.WriteLine("! Invalid number");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(students[id]))
+            {
+                Console.WriteLine("! Invalid student ID");
+                return;
+            }
+
+            students[id] = null;
+            Console.WriteLine($"< '{id}' deleted");
+        }
+
+        public static void CountStudents()
+        {
+            int count = 0;
+
+            foreach (var student in students)
+            {
+                if (!string.IsNullOrEmpty(student))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"< {count}");
         }
     }
 }
