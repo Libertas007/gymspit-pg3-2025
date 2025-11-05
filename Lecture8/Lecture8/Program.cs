2280cfa383bb81061cbab3232e914157d47d3fbb
Libertas007 <1adamsvoboda@gmail.com>
2025-11-05 09:27:40 +0100

    unit tests 🤢

---
+++ Lecture8/Lecture8/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Lecture8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public static double SerialCapacitors(double[] capacitances)
+        {
+            double C = 0.0;
+
+            foreach (double C0 in capacitances)
+            {
+                C += 1 / C0;
+            }
+
+            return 1 / C;
+        }
+
+        public static double ParallelCapacitors(double[] capacitances)
+        {
+            double C = 0.0;
+
+            foreach (double C0 in capacitances)
+            {
+                C += C0;
+            }
+
+            return C;
+        }
+    }
+}
