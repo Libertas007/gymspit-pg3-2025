c3ed5109d0518dc0355a924a56a36992a08b03af
Libertas007 <1adamsvoboda@gmail.com>
2025-12-10 09:42:45 +0100

    continued working on it

--- Lecture10/Lecture10/Program.cs
+++ Lecture10/Lecture10/Program.cs
@@ -1,4 +1,7 @@
-﻿// tym: Sam Kouklik a Adam Svoboda
+﻿// tym: Sam Kouklik, Adam Svoboda a Ondrej Stindl
+
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
 
 namespace Lecture10
 {
@@ -6,27 +9,45 @@ namespace Lecture10
     {
         public static void Main(string[] args)
         {
+            TerminalUI.PrintLogo();
+            string path = TerminalUI.GetFilePrompt();
+
             Twitter twitter = new Twitter();
+
+            if (!string.IsNullOrEmpty(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string json = sr.ReadToEnd();
+                    twitter.LoadData(json);
+                    twitter.filePath = path;
+                }
+            }
+
+            TerminalUI.ClearAndLogo();
+
+            TerminalUI.PrintPost(new Post { Author = "libertas", Content = "asdgajlhdbqwuihbudlihasbluidhasl" });
         }
     }
 
     class Twitter
     {
+        public string filePath = "";
+
         const int MAX_USERS = 100;
         const int MAX_POSTS = MAX_USERS * 100;
         const int MAX_FOLLOWS = MAX_USERS * (MAX_USERS + 1) / 2;
 
-        User[] users = new User[MAX_USERS];
-        Post[] posts = new Post[MAX_POSTS];
-        Follows[] follows = new Follows[MAX_FOLLOWS];
+        User?[] users = new User[MAX_USERS];
+        Post?[] posts = new Post[MAX_POSTS];
+        Follow?[] follows = new Follow[MAX_FOLLOWS];
 
-        private bool AddValue<T>(T value, ref T[] array, int index = -1) where T : WithId
+        private bool AddValue<T>(ref T?[] array, T value, int index = -1) where T : WithId
         {
-            index = index == -1 ? Array.FindIndex(array, v => string.IsNullOrEmpty(v.ToString())) : index;
+            index = index == -1 ? Array.FindIndex(array, v => string.IsNullOrEmpty(v?.ToString())) : index;
 
             if (array[index] == null) 
             {
-                Console.WriteLine("Index occupied.");
                 return false;
             }
 
@@ -36,42 +57,58 @@ namespace Lecture10
             return true;
         }
 
-        private bool RemoveValue<T>(int index, ref T[] array) where T: new()
+        private bool RemoveValue<T>(ref T?[] array, int index) where T: class
         {
             if (index < 0 || index >= array.Length)
             {
-                Console.WriteLine("Invalid index.");
                 return false;
             }
 
-            array[index] = default(T);
+            array[index] = null;
             return true;
+        }
+
+        private bool RemoveWhere<T>(ref T?[] array, Predicate<T?> predicate) where T : class
+        {
+            int index = Array.FindIndex(array, predicate);
+
+            return RemoveValue(ref array, index);
+        }
+
+        private bool Exists<T>(ref T?[] array, Func<T?, bool> predicate) where T : WithId
+        {
+            return array.Any(predicate);
         }
 
         public void AddUser(string username)
         {
-            int index = Array.IndexOf(users, username);
-            if (index >= 0)
+            if (Exists(ref users, (u) => u != null && u.Name == username))
             {
-                Console.WriteLine("User already exists.");
+                PrintErrorMessage("User already exists.");
                 return;
             }
             
             User user = new User() { Name = username };
 
-            AddValue(user, ref users);
+            if (!AddValue(ref users, user))
+            {
+                PrintErrorMessage("User array full.");
+            }
         }
 
         public void RemoveUser(string username)
         {
-            int index = Array.IndexOf(users, username);
-            if (index < 0)
+            if (!Exists(ref users, (u) => u != null && u.Name == username))
             {
-                Console.WriteLine("User does not exist.");
+                PrintErrorMessage("User does not exist.");
                 return;
             }
 
-            if (index >= 0) RemoveValue(index, ref users);
+            
+            if (RemoveWhere(ref users, (u) => u.Name == username))
+            {
+                PrintErrorMessage("Invalid index.");
+            }
         }
 
 
@@ -79,80 +116,145 @@ namespace Lecture10
         {
             Post post = new Post() { Content = content, Author = author };
 
-            AddValue(post, ref posts);
+            AddValue(ref posts, post);
         }
 
-        public Post[] GetUserPosts(string user)
+        public Post?[] GetUserPosts(string user)
         {
-            return Array.FindAll(posts, p => p.Author == user);
+            return Array.FindAll(posts, p => p != null && p.Author == user);
         }
 
 
         public void AddFollow(string follower, string followee)
         {
-            // TODO
+            Follow follow = new Follow()
+            {
+                Follower = follower,
+                Followee = followee,
+            };
+
+            if (follower == followee)
+            {
+                PrintErrorMessage("Cannot follow yourself.");
+                return;
+            }
+
+            if (!Exists(ref users, (u) => u != null && (u.Name == follower || u.Name == followee))) {
+                PrintErrorMessage("Invalid follower or followee.");
+                return;
+            }
+
+            if (Exists(ref follows, (f) => f != null && f.Followee == followee && f.Follower == follower))
+            {
+                PrintErrorMessage("Follow already exists.");
+                return;
+            }
+
+            AddValue(ref follows, follow);
         }
 
         public void RemoveFollow(string follower, string followee)
         {
-            // TODO
+            if (!Exists(ref follows, (f) => f != null && f.Followee == followee && f.Follower == follower));
+            {
+                PrintErrorMessage("Follow does not exist.");
+                return;
+            }
         }
 
-        public string[] GetUserFollows(string user)
+        public Follow?[] GetUserFollows(string user)
         {
-            // TODO
-            return new string[] { };
+            return [.. follows.Where((f) => f != null && f.Follower == user)];
         }
 
-        string[] GetUserFollowers(string user)
+        public Follow?[] GetUserFollowers(string user)
         {
-            // TODO
-            return new string[] { };
+            return [.. follows.Where((f) => f != null && f.Followee == user)];
         }
 
 
         // Bonus
-        public string[] GetUserTimeline(string user)
+        public WithId?[] GetUserTimeline(string user)
         {
             // TODO
-            return new string[] { };
+            return new WithId[] { };
         }
 
-        public abstract record class WithId
+        public Post?[] GetRecentPosts(int number)
         {
-            public int Id;
+            return [.. posts.TakeLast(number)];
         }
 
-        public record class Post : WithId
+        public string Serialize()
         {
-            public string Content = "";
-            public string Author = "";
+            return JsonSerializer.Serialize(new JSON { Follows = follows, Posts = posts, Users = users });
+        }
 
-            public override string ToString()
+        public void LoadData(string json)
+        {
+            var doc = JsonSerializer.Deserialize(json, JsonTypeInfo.CreateJsonTypeInfo<JSON>(JsonSerializerOptions.Default));
+
+            if (doc == null)
             {
-                return Content;
+                PrintErrorMessage("Could not parse JSON.");
+                return;
             }
+
+            posts = doc.Posts;
+            users = doc.Users;
+            follows = doc.Follows;
         }
 
-        public record class User : WithId
+        private void PrintErrorMessage(string message)
         {
-            public string Name = "";
-
-            public override string ToString()
-            {
-                return Name;
-            }
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"ERROR! {message}");
+            Console.ResetColor();
         }
 
-        public record class Follows : WithId
+        private record class JSON
         {
-            public string Follower = "";
-            public string Followee = "";
+            required public Post[] Posts;
+            required public User[] Users;
+            required public Follow[] Follows;
+        }
+    }
 
-            public override string ToString()
-            {
-                return Follower;
-            }
+    public abstract record class WithId
+    {
+        public int Id;
+    }
+
+    public record class Post : WithId
+    {
+        public string Content = "";
+        public string Author = "";
+
+        public override string ToString()
+        {
+            return Content;
+        }
+    }
+
+    public record class User : WithId
+    {
+        public string Name = "";
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public record class Follow : WithId
+    {
+        public string Follower = "";
+        public string Followee = "";
+
+        public override string ToString()
+        {
+            return Follower;
         }
     }
 }
---
+++ Lecture10/Lecture10/TerminalUI.cs
@@ -0,0 +1,44 @@
+﻿namespace Lecture10
+{
+    public class TerminalUI
+    {
+        public static void PrintLogo()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("   __           _ __  __           \r\n  / /__      __(_) /_/ /____  _____\r\n / __/ | /| / / / __/ __/ _ \\/ ___/\r\n/ /_ | |/ |/ / / /_/ /_/  __/ /    \r\n\\__/ |__/|__/_/\\__/\\__/\\___/_/     \r\n                                   ");
+            Console.WriteLine();
+            Console.ResetColor();
+        }
+
+        public static string GetFilePrompt()
+        {
+            Console.WriteLine("Enter the file name of the instance, or write 'temp' for a temporary instance.");
+            Console.Write("> ");
+            string path = (Console.ReadLine() ?? "").Trim();
+
+            return path == "temp" ? "" : path;
+        }
+
+        public static void ClearAndLogo()
+        {
+            Console.Clear();
+            PrintLogo();
+        }
+
+        public static void PrintPost(Post post)
+        {
+            Console.WriteLine($"+{new string('-', Console.WindowWidth - 2)}+");
+            Console.WriteLine($"| @{post.Author} {new string(' ', Console.WindowWidth - 2 - 3 - post.Author.Length)}|");
+            Console.WriteLine($"| {post.Content} {new string(' ', Console.WindowWidth - 2 - 2 - post.Content.Length)}|");
+            Console.WriteLine($"+{new string('-', Console.WindowWidth - 2)}+");
+            Console.WriteLine();
+        }
+
+        public static string LoginPrompt()
+        {
+            Console.WriteLine("Enter your username:");
+            Console.Write("> ");
+            return (Console.ReadLine() ?? "").Trim();
+        }
+    }
+}
